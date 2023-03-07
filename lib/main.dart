b5e39ada640b141f68b578bda36a1ad45b94166a
Riko Airlan Ramadhan <rikoairlan57@gmail.com>
2023-03-07 15:32:33 +0700

    💄 🎨 crested sample ui register

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:story_app/screen/login_screen.dart';
+
+import 'package:story_app/screen/register_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: LoginScreen(),
+      home: RegisterScreen(),
     );
   }
 }
