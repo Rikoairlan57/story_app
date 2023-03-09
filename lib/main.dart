111ca0ff71b32056e0551fd820087a7df1993cb0
Riko Airlan Ramadhan <rikoairlan57@gmail.com>
2023-03-09 12:37:39 +0700

    💄 ✅ ✨ ⚡️ 🎨 fetching data home screen story list

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,57 @@
 import 'package:flutter/material.dart';
-import 'package:story_app/ui/screen/register_screen.dart';
+import 'package:provider/provider.dart';
+import 'package:story_app/data/api/api_service.dart';
+import 'package:story_app/data/db/auth_repository.dart';
+import 'package:story_app/provider/auth_provider.dart';
+import 'package:story_app/provider/create_story_provider.dart';
+import 'package:story_app/provider/story_list_provider.dart';
+import 'package:story_app/routes/page_manager.dart';
+import 'package:story_app/routes/route_delegate.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late MyRouteDelegate myRouteDelegate;
+  late AuthProvider authProvider;
+  final authRepository = AuthRepository();
+
+  @override
+  void initState() {
+    super.initState();
+
+    authProvider = AuthProvider(authRepository, ApiService());
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Story App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => authProvider,
+        ),
+        ChangeNotifierProvider(create: (context) => PageManager()),
+        ChangeNotifierProvider(
+            create: (context) => StoryListProvider(apiService: ApiService())),
+        ChangeNotifierProvider(
+            create: (context) => CreateStoryProvider(ApiService())),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Story App',
+        home: Router(
+          routerDelegate: myRouteDelegate,
+          backButtonDispatcher: RootBackButtonDispatcher(),
+        ),
       ),
-      home: RegisterScreen(),
     );
   }
 }
